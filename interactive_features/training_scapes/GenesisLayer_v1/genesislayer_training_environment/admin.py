bd5ed92716fafdcf6133ef9012c9da500637d0d3
xd3v0nx <careybrands@gmail.com>
2023-06-13 22:54:03 -0400

    Integrated admin file

--- interactive_features/training_scapes/GenesisLayer_v1/genesislayer_training_environment/admin.py
+++ interactive_features/training_scapes/GenesisLayer_v1/genesislayer_training_environment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, AIModel, InterfaceLayer, TrainingSession, CursorMovement, TrainingScape, CursorCustomization, Feedback, SessionHistory
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(AIModel)
+admin.site.register(InterfaceLayer)
+admin.site.register(TrainingSession)
+admin.site.register(CursorMovement)
+admin.site.register(TrainingScape)
+admin.site.register(CursorCustomization)
+admin.site.register(Feedback)
+admin.site.register(SessionHistory)
