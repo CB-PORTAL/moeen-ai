7eeeb88258828bfc09099b7e8528877e8ad96dae
xd3v0nx <careybrands@gmail.com>
2023-06-29 22:17:04 -0400

    updated app.js from src

---
+++ interactive_features/training_scapes/GenesisLayer_v1_Frontend(react)/genesislayer_v1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SmartExpandableWindowComponent from './SmartExpandableWindowComponent';
+import OverlayComponent from './OverlayComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SmartExpandableWindowComponent />
+      <OverlayComponent />
+    </div>
+  );
+}
+
+export default App;
